d598cf0e8cdc8958a21f244420786f430c8ec493
¨mateusdiias¨ <mateusdb30@gmail.com>
2023-08-01 11:37:19 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,8 +1,6 @@
 package boardgame;
 
-import com.sun.jdi.connect.Connector;
-
-public class Piece {
+public abstract class Piece {
     protected Position position;
     private Board board;
 
@@ -13,5 +11,28 @@ public class Piece {
 
     protected Board getBoard() {
         return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    //  Testar se uma peça pode mover para uma dada posição
+//  método concreto que utiliza um método abstrato(hook method). retorna o método
+//  possibleMoves, nas linhas e colunas da classe Position
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    //  método que verifica se existe pelomenos um movimento possível para
+//  determinada peça. Útil para ver se a peça está travada, sem movimento.
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
